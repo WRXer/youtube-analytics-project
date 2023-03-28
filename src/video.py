1aba12ef1e5d2eed0cfba691a11585000ff22957
WRXer <king_311@mail.ru>
2023-03-28 18:56:28 +0400

    Внедрил в код блоки try/except , чтобы отловить исключение HttpError.

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video():
@@ -10,16 +11,25 @@ class Video():
     youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, video_id):
         self.__video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.__video_id).execute()
-        self.video = self.youtube.channels().list(id=self.__video_id, part='snippet,statistics').execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url = f"https://youtu.be/{self.__video_id}"
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        #print(self.video_response['items'])
+        self.title = None
+        self.url = None
+        self.view_count = None
+        self.like_count = None
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.__video_id).execute()
+            self.video = self.youtube.channels().list(id=self.__video_id, part='snippet,statistics').execute()
+            items = self.video_response.get('items', [])
+            if items:
+                self.title: str = self.video_response['items'][0]['snippet']['title']
+                self.url = f"https://youtu.be/{self.__video_id}"
+                self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+                self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            #print(self.video_response['items'])
+        except HttpError as e:
+            print(f'An error occurred: {e}')
 
     def __str__(self):
-        return f"{self.video_title}"
+        return f"{self.title}"
 
 
 class PLVideo(Video):
@@ -28,4 +38,4 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
 
     def __str__(self):
-        return f"{self.video_title}"
+        return f"{self.title}"
